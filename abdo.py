6d76d1092824c1f4bab062283b092c95d539a366
abdelrahman <abdelrahmaneldaba123@gmail.com>
2023-02-06 14:42:18 +0200

    removing libraries

--- abdo.py
+++ abdo.py
@@ -1,7 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
